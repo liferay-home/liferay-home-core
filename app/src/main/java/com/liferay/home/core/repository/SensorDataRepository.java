0d00ea1516fb16b3e0c863b2039765994882e114
akosthurzo <akos.thurzo@liferay.com>
2017-01-27 12:00:46 -0800

    Add SensorData repo

---
+++ app/src/main/java/com/liferay/home/core/repository/SensorDataRepository.java
@@ -0,0 +1,12 @@
+package com.liferay.home.core.repository;
+
+import com.liferay.home.core.model.SensorData;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * @author Akos Thurzo
+ */
+@RepositoryRestResource
+public interface SensorDataRepository extends CrudRepository<SensorData, Long> {
+}
