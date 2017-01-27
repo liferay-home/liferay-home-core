443f23c221a8dd7dcaa7fb90839755922753908b
danielkocsis <daniel.kocsis@liferay.com>
2017-01-27 12:57:53 -0800

    test4

---
+++ app/src/main/java/com/liferay/home/core/com/liferay/home/core/Application.java
@@ -0,0 +1,18 @@
+package com.liferay.home.core.com.liferay.home.core;
+
+import com.liferay.home.core.model.SensorData;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableJpaRepositories(basePackageClasses = SensorData.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
--- app/src/main/java/io/wedeploy/example/WeDeployController.java
+++ app/src/main/java/io/wedeploy/example/WeDeployController.java
@@ -1,27 +1,11 @@
 package io.wedeploy.example;
 
-import com.liferay.home.core.model.SensorData;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@EnableAutoConfiguration
-@EnableJpaRepositories(basePackageClasses = SensorData.class)
 public class WeDeployController {
-
-    public WeDeployController() {
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println("\n\n ################ Starter");
-
-        SpringApplication.run(WeDeployController.class, args);
-    }
 
     @RequestMapping("/")
     public ModelAndView hello() {
